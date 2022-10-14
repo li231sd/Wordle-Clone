7ba6e541613867bc99c8ed09bc1fc8c30a704a12
li231sd <113144462+li231sd@users.noreply.github.com>
2022-10-13 18:07:42 -0700

    Update targetWords.js

--- targetWords.js
+++ targetWords.js
@@ -1,36 +1,6 @@
 const targetWords = [
-  "holks",
-  "holla",
-  "hollo",
-  "holme",
-  "holms",
-  "holon",
-  "holos",
-  "holts",
-  "homas",
-  "homed",
-  "homes",
-  "homey",
-  "homie",
-  "homme",
-  "homos",
-  "honan",
-  "rebut",
-  "humph",
-  "awake",
-  "blush",
-  "focal",
-  "evade",
-  "naval",
-  "serve",
-  "heath",
-  "dwarf",
-  "model",
-  "karma",
-  "stink",
-  "grade",
-  "quiet",
-  ];
+  "holks","holla","hollo","holme","holms","holon","holos","holts","homas","homed","homes","homey","homie","homme","homos","honan","rebut","humph","awake","blush","focal","evade","naval","serve","heath","dwarf","model","karma","stink","grade","quiet",
+];
   
-  export default targetWords;
+export default targetWords;
   
