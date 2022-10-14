9221079047ca4f6d2987f8a10b28d38b7eb9cc0f
li231sd <113144462+li231sd@users.noreply.github.com>
2022-10-13 17:56:13 -0700

    Add files via upload

---
+++ targetWords.js
@@ -0,0 +1,20 @@
+const targetWords = [
+    "rebut",
+    "humph",
+    "awake",
+    "blush",
+    "focal",
+    "evade",
+    "naval",
+    "serve",
+    "heath",
+    "dwarf",
+    "model",
+    "karma",
+    "stink",
+    "grade",
+    "quiet",
+  ];
+  
+  export default targetWords;
+  
